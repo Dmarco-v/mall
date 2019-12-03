7adfcb3be718743f8039a37ed0ff713b73e140c9
dmarco <yejingxiong@foxmail.com>
2019-12-03 11:20:04 +0800

    update seckill project

---
+++ seckill/src/main/java/com/seckillproject/App.java
@@ -0,0 +1,39 @@
+package com.seckillproject;
+
+import com.seckillproject.dao.UserDOMapper;
+import com.seckillproject.dataObject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages = {"com.seckillproject"})
+@RestController
+@MapperScan("com.seckillproject.dao")
+public class App 
+{
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/")
+    public String home(){
+        UserDO userDO=userDOMapper.selectByPrimaryKey(1);
+        if(userDO==null){
+            return "对象用户不存在";
+        }else {
+            return userDO.getName();
+        }
+    }
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
